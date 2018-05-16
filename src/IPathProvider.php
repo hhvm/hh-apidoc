7207391c0e6e6dbfde92f5f61d7d75333eb60053
Fred Emmott <fred@fredemmott.co.uk>
2018-05-16 14:49:05 -0700

    document path providers (#23)
    * document path providers
    
    * decided => decide

--- src/IPathProvider.php
+++ src/IPathProvider.php
@@ -10,17 +10,34 @@
 
 namespace Facebook\HHAPIDoc;
 
+/** Get paths for documentable definitions.
+ *
+ * These are used to:
+ * - generate links
+ * - decide where to create files containing documentation
+ *
+ * If `T` is nullable, it should return null if the definition does not exist.
+ *
+ * @see IndexedPathProvider
+ */
 interface IPathProvider<+T as ?string> {
+  /** @selfdocumenting */
   public function getPathForClass(string $class): T;
+  /** @selfdocumenting */
   public function getPathForInterface(string $interface): T;
+  /** @selfdocumenting */
   public function getPathForTrait(string $trait): T;
 
+  /** @selfdocumenting */
   public function getPathForClassMethod(string $class, string $method): T;
+  /** @selfdocumenting */
   public function getPathForInterfaceMethod(
     string $interface,
     string $method,
   ): T;
+  /** @selfdocumenting */
   public function getPathForTraitMethod(string $trait, string $method): T;
 
+  /** @selfdocumenting */
   public function getPathForFunction(string $function): T;
 }
