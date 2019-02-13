f0042155dbca753d972560b1e87bd63c116596b5
Fred Emmott <fe@fb.com>
2019-02-13 12:54:26 -0800

    Use Docker to test on Travis

--- .travis.sh
+++ .travis.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
+php --version
 
-composer install --ignore-platform-reqs
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
+composer install
 
 hh_client
+hhvm vendor/bin/hacktest tests/
+if !(hhvm --version | grep -q -- -dev); then
+  hhvm vendor/bin/hhast-lint
+fi
 
-# Make sure it 'works'
-hhvm bin/hh-apidoc -o docs src/
-# Make sure it lints
-hhvm vendor/bin/hhast-lint
-
+hhvm bin/hh-apidoc -o docs src
 git add docs/
 if ! git diff --quiet --cached; then
   echo "Documentation needs regenerating."
-  exit 1
-fi
+ exit 1
