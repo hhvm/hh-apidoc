c059ec6b055f6272c7992ddd7a780feaa685fd1d
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2021-01-06 19:26:16 +0100

    Run composer's version command on CI
    This is how composer checks hhvm's version.
    This should be the same in all environments for a given version.
    This is not meant to be merged.

--- .travis.sh
+++ .travis.sh
@@ -9,6 +9,7 @@ php --version
   cd $(mktemp -d)
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version="1.9.3"
 )
+hhvm --php -d hhvm.jit=0 -r "echo HHVM_VERSION;"
 composer install
 
 hh_client
